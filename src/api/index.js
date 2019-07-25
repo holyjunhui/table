51fbe1731a8d3c5dd365c7015fb31fbde26e8863
Baipan <baipan@360.cn>
2019-07-25 16:18:12 +0800

    [clould-detection] feat: 添加 API 接口

--- src/api/index.js
+++ src/api/index.js
@@ -30,8 +30,30 @@ service.interceptors.response.use(response => {
     return Promise.reject(new Error("请求接口失败: " + msg));
 });
 
+const getMeta = () => service.get("/v1/meta");
 const getAlertsTotal = () => service.get("/v1/situation/alerts/total");
+const getAlertsStatsByLocation = locationCode => service.get(`/v1/situation/alerts/stats-by-location/${locationCode}`);
+const getAlertsRecent = range => service.get("/v1/situation/alerts/recent", {params: {range}});
+const getAlertsHighSeverity = () => service.get("/v1/situation/alerts/high-severity");
+const getAlertsHighSeveritySummary = () => service.get("/v1/situation/alerts/high-severity/summary");
+const getAlertsSpamlink = () => service.get("/v1/situation/alerts/spamlink");
+const getAlertsRank = location_code => service.get("/v1/situation/alerts/rank", {params: {location_code}});
+const getAlertsSummaryToday = () => service.get("/v1/situation/alerts/summary-today");
+const getAssetsRecent = () => service.get("/v1/situation/assets/recent");
+const getAssetsSummary = () => service.get("/v1/situation/assets/summary");
+const getOperationSummary = () => service.get("/v1/situation/operation/summary");
 
 export {
-    getAlertsTotal
+    getMeta,
+    getAlertsTotal,
+    getAlertsStatsByLocation,
+    getAlertsRecent,
+    getAlertsHighSeverity,
+    getAlertsHighSeveritySummary,
+    getAlertsSpamlink,
+    getAlertsRank,
+    getAlertsSummaryToday,
+    getAssetsRecent,
+    getAssetsSummary,
+    getOperationSummary
 };
