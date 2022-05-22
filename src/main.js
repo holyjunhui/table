11cae8209de85d5b895d0eb01096fb0224371cc3
lijunhui <jh.li@yamu.com>
2022-05-22 15:20:51 +0800

    fix: dele config

--- src/main.js
+++ src/main.js
@@ -11,6 +11,6 @@ Vue.prototype.dayjs = dayjs;
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,37 @@
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
-  pages: {
-    index: {
-      entry: "src/main.js",
-      template: "public/index.html",
-      filename: "index.html"
-    }
-  },
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === "production") {
-      config.plugins.push(new UglifyJsPlugin());
-      // config.optimization.minimizer.push(new UglifyJsPlugin());
-    }
-  },
-
-  chainWebpack(config) {
-    config.plugins.delete("preload-index");
-    config.plugins.delete("prefetch-index");
-  },
-
-  devServer: {
-    open: "true",
-    proxy: {
-      "/jinshan": {
-        target: "http://123.206.122.198:11880/", // 这里后台的地址
-        ws: false,
-        changOrigin: true, // 允许跨域
-        pathRewrite: {
-          "^/jinshan": "" // 请求的时候使用这个api就可以
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+    pages: {
+        index: {
+            entry: "src/main.js",
+            template: "public/index.html",
+            filename: "index.html"
         }
-      }
-    }
+    },
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === "production") {
+            config.plugins.push(new UglifyJsPlugin());
+            // config.optimization.minimizer.push(new UglifyJsPlugin());
+        }
+    },
+
+    chainWebpack(config) {
+        config.plugins.delete("preload-index");
+        config.plugins.delete("prefetch-index");
+    },
+
+    devServer: {
+        open: "true",
+        proxy: {
+            "/jinshan": {
+                target: "http://123.206.122.198:11880/", // 这里后台的地址
+                ws: false,
+                changOrigin: true, // 允许跨域
+                pathRewrite: {
+                    "^/jinshan": "" // 请求的时候使用这个api就可以
+                }
+            }
+        }
     // proxy: {// 配置跨域
     //     "/v1": {
     //         target: "http://10.145.79.28:8080/api", // 这里后台的地址
@@ -42,5 +42,5 @@ module.exports = {
     //         }
     //     }
     // }
-  }
+    }
 };
